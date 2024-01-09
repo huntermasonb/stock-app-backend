ea2485ccba4468740a4a611a24578fbb0e23e1d6
Hunter M <huntermason15@gmail.com>
2024-01-09 16:54:29 -0500

    Version 0.0.1 - Re-styling the header for the dashboard after a user has signed in, the desktop menu and mobile menus, and the dashboard page as a whole to match my color scheme.

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -10,18 +10,18 @@ export default function Authenticated({ user, header, children }) {
 
     return (
         <div className="min-h-screen bg-gray-100">
-            <nav className="bg-white border-b border-gray-100">
+            <nav className="bg-indigo-900 border-b border-indigo-100">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex justify-between h-16">
                         <div className="flex">
                             <div className="shrink-0 flex items-center">
                                 <Link href="/">
-                                    <ApplicationLogo className="block h-9 w-auto fill-current text-indigo-700" />
+                                    <ApplicationLogo className="block h-9 w-auto fill-current text-indigo-400" />
                                 </Link>
                             </div>
 
                             <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                <NavLink className="text-indigo-900" href={route('dashboard')} active={route().current('dashboard')}>
+                                <NavLink className="text-indigo-100" href={route('dashboard')} active={route().current('dashboard')}>
                                     Dashboard
                                 </NavLink>
                             </div>
@@ -34,7 +34,7 @@ export default function Authenticated({ user, header, children }) {
                                         <span className="inline-flex rounded-md">
                                             <button
                                                 type="button"
-                                                className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-indigo-900 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                                                className="inline-flex bg-[#D2D5EF] items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-indigo-900 focus:outline-none transition ease-in-out duration-150"
                                             >
                                                 {user.name}
 
